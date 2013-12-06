a01d5d84a272d9cbd9a63ce504fd568512ae234f
Orlando Del Aguila <orlando@delagui.la>
2013-12-06 00:28:09 -0600

    changing package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,7 @@
 #
 #   include ade
 class ade {
-  package { 'ADE':
-    ensure   => 'installed',
+  package { 'ADE 2.0':
     provider => 'pkgdmg',
     source   => 'http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg'
   }
--- spec/classes/ade_spec.rb
+++ spec/classes/ade_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe 'ade' do
   it do
-    should contain_package('ADE').with({
-      :ensure   => 'installed',
+    should contain_package('ADE 2.0').with({
       :source   => 'http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg',
       :provider => 'pkgdmg'
     })
